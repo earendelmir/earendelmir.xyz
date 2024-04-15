84d080524e7c272d532aadd0deb596058dfb3f1a
earendelmir <earendelmir@proton.me>
2024-04-15 20:24:07 +0200

    js theme switch supports auto theme ; small

--- docs/scripts/theme.js
+++ docs/scripts/theme.js
@@ -1,28 +1,43 @@
-function setTheme(dark, save=false) {
-  if (dark) document.documentElement.classList.add("dark")
-  else document.documentElement.classList.remove("dark")
-  if (save) localStorage.setItem("theme", dark ? "dark" : "light")
+function setTheme(theme, save=false, noauto=true) {
+  if (theme === "dark") {
+    document.documentElement.classList.add("dark")
+    document.documentElement.classList.remove("light")
+    if (noauto) document.documentElement.classList.remove("auto")
+    if (save) localStorage.setItem("theme", theme)
+  } else if (theme === "light") {
+    document.documentElement.classList.add("light")
+    document.documentElement.classList.remove("dark")
+    if (noauto) document.documentElement.classList.remove("auto")
+    if (save) localStorage.setItem("theme", theme)
+  } else if (theme === "auto") {
+    document.documentElement.classList.add("auto")
+    document.documentElement.classList.remove("light")
+    document.documentElement.classList.remove("dark")
+    localStorage.removeItem("theme")
+    setTheme(q.matches ? "dark" : "light", false, false)
+  } else return
   try {
-    setText(dark)
-    document.querySelector('meta[name="theme-color"]').setAttribute("content", dark ? "#18191A" : "#F8F9FA")
+    document.querySelector('meta[name="theme-color"]').setAttribute("content", theme === "dark" ? "#18191A" : "#F8F9FA")
+    if (save) setText(theme)
   }
   catch {}
 }
-function setText(dark) {
-  document.getElementById("btn-theme").innerText = dark ? "light" : "dark"
-}
+function setText(theme){document.getElementById("btn-theme").innerText=theme}
 
 const q = window.matchMedia("(prefers-color-scheme: dark)")
-const initialDark = localStorage.getItem("theme") === "dark" || (!("theme" in localStorage) && q.matches)
-setTheme(initialDark)
+const initialTheme = localStorage.getItem("theme") || "auto"
+setTheme(initialTheme, true, false)
 
 window.onload = function(){
-  setText(initialDark)
+  setText(initialTheme)
   q.addEventListener("change", function(){
-    if (!("theme" in localStorage))
-      setTheme(q.matches)
+    if (!("theme" in localStorage)) setTheme(q.matches ? "dark" : "light", false, false)
   });
   document.getElementById("btn-theme").addEventListener("click", function(){
-    setTheme(!document.documentElement.classList.contains("dark"), true)
+    classes=document.documentElement.classList;
+    if (classes.contains("auto")) newTheme="dark"
+    else if (classes.contains("dark")) newTheme="light"
+    else if (classes.contains("light")) newTheme="auto"
+    setTheme(newTheme, true, newTheme !== "auto")
   });
 };
