4caa9f3cdcfa275e418c2461ff2f174ce99ab4d2
earendelmir <earendelmir@proton.me>
2024-04-18 10:58:43 +0200

    small

--- scripts/update-sitemap-lastmod.sh
+++ scripts/update-sitemap-lastmod.sh
@@ -3,7 +3,7 @@
 # File:   update-sitemap-lastmod.sh
 # Author: Marco Plaitano
 # Date:   13 Apr 2024
-# Brief:  Update lastmod date for every entry in the XML sitemap to current day.
+# Brief:  Update lastmod date for every entry in the XML sitemap.
 
 set -eo pipefail
 
@@ -18,7 +18,7 @@ _die() {
 
 _help() {
     printf "Usage: %s
-Update lastmod date for every entry in the XML sitemap to current day.\n" \
+Update lastmod date for every entry in the XML sitemap.\n" \
 "$(basename "$0")"
 }
 
@@ -39,6 +39,23 @@ done
 LANG=en_us_8859_1
 readonly _FILE_SITEMAP_XML="sitemap-xml.xml"
 
-new_line="    <lastmod>$(date +'%Y-%m-%d')</lastmod>"
+grep "<loc" "$_FILE_SITEMAP_XML" 2>/dev/null | while IFS= read -r line ; do
+    filename="$(echo "$line" | cut -d'>' -f2 | cut -d'<' -f1)"
+    # Get number of line to replace.
+    line_nr="$(grep -n -m 1 "$filename" "$_FILE_SITEMAP_XML" | cut -d':' -f1)"
+    line_nr=$((line_nr + 1))
 
-sed -i "/^    <lastmod>/c\\$new_line" "$_FILE_SITEMAP_XML"
+    # Get actual file path, to check its last modification date.
+    file="${filename#*xyz/}"
+    if [[ "$file" =~ [0-9]$ ]]; then
+        file+=".html"
+    else
+        file+="index.html"
+    fi
+    if [[ ! -f "$file" ]]; then
+        echo "File $file from sitemap.xml not found" >&2
+        continue
+    fi
+    new_line="    <lastmod>$(date -r "$file" +'%Y-%m-%d')</lastmod>"
+    sed -i "${line_nr}s|.*|${new_line}|" "$_FILE_SITEMAP_XML"
+done
